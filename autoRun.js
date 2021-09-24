da431a0170168258caf14c227ee97de361951ffe
Ryan C <Ryancrispin@gmail.com>
2021-09-24 13:08:09 -0400

    added MLB and NFL and changed to google geocoding

--- autoRun.js
+++ autoRun.js
@@ -10,12 +10,12 @@ PythonShell.run('./Scripts/NBA_scrapping.py', null, function (err) {
   console.log('NBA finished');
 });
 
-// PythonShell.run('./Scripts/NFL_scrapping.py', null, function (err) {
-//   if (err) throw err;
-//   console.log('NFL finished');
-// });
+PythonShell.run('./Scripts/NFL_scrapping.py', null, function (err) {
+  if (err) throw err;
+  console.log('NFL finished');
+});
 
-// PythonShell.run('./Scripts/MLB_scrapping.py', null, function (err) {
-//   if (err) throw err;
-//   console.log('MLB finished');
-// });
+PythonShell.run('./Scripts/MLB_scrapping.py', null, function (err) {
+  if (err) throw err;
+  console.log('MLB finished');
+});
